5480761946f7d600d3dba71b8d9cbe2bd2587794
Petro Kuzmychev <johnnyfearmekramer@gmail.com>
2020-09-16 22:13:14 +0400

    Style snackbar

--- src/darkTheme/darkTheme.jsx
+++ src/darkTheme/darkTheme.jsx
@@ -12,6 +12,18 @@ const PTSansFont = {
     'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
 }
 
+const snackbarColors = {
+  title: '#000',
+  subtitle: '#000',
+  background: '#fff',
+  infoIcon: '#000',
+  warnIcon: '#000',
+  errorIcon: '#000',
+  hashIcon: '#000',
+  successIcon: '#000',
+  closeIcon: '#000',
+}
+
 export const colors = {
   beta: {
     bg: 'radial-gradient(22.99% 100% at 50.42% 100%, rgba(64, 169, 255, 0.3) 0%, rgba(64, 169, 255, 0) 100%), #293140;',
@@ -178,7 +190,7 @@ export const colors = {
       },
     },
   },
-  white: '#654321',
+  white: '#fff',
   black: '#000',
   darkBlue: '#2c3b57',
   blue: '#2F80ED',
@@ -352,7 +364,7 @@ const iswapTheme = {
     },
     MuiSnackbarContent: {
       root: {
-        backgroundColor: colors.white,
+        backgroundColor: snackbarColors.background,
         padding: '0px',
         minWidth: 'auto',
         '@media (min-width: 960px)': {
@@ -466,6 +478,7 @@ const iswapTheme = {
   },
   breakpoints: breakpoints,
   themeColors: colors,
+  snackbar: snackbarColors,
 }
 
 export default iswapTheme
--- src/waifuTheme/waifuTheme.jsx
+++ src/waifuTheme/waifuTheme.jsx
@@ -12,6 +12,18 @@ const PTSansFont = {
     'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
 }
 
+const snackbarColors = {
+  title: '#000',
+  subtitle: '#000',
+  background: '#ffede7',
+  infoIcon: '#000',
+  warnIcon: '#000',
+  errorIcon: '#000',
+  hashIcon: '#000',
+  successIcon: '#000',
+  closeIcon: '#000',
+}
+
 export const colors = {
   beta: {
     bg: '#F3F4F8;',
@@ -177,7 +189,7 @@ export const colors = {
       },
     },
   },
-  white: '#ffede7',
+  white: '#fff',
   black: '#000',
   darkBlue: '#2c3b57',
   blue: '#2F80ED',
@@ -351,7 +363,7 @@ const iswapTheme = {
     },
     MuiSnackbarContent: {
       root: {
-        backgroundColor: colors.white,
+        backgroundColor: snackbarColors.background,
         padding: '0px',
         minWidth: 'auto',
         '@media (min-width: 960px)': {
@@ -465,6 +477,7 @@ const iswapTheme = {
   },
   breakpoints: breakpoints,
   themeColors: colors,
+  snackbar: snackbarColors,
 }
 
 export default iswapTheme
