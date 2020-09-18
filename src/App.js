e3724c742d43cf67e3736c2e1d5b2c999658e919
Petro Kuzmychev <johnnyfearmekramer@gmail.com>
2020-09-19 01:57:03 +0400

    fix router

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
-import React, { Component } from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
-import { Switch, Route } from "react-router-dom";
-import IpfsRouter from "ipfs-react-router";
-import { withNamespaces } from "react-i18next";
+import React, { Component } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import IpfsRouter from 'ipfs-react-router'
+import { withNamespaces } from 'react-i18next'
 
-import "./i18n";
-import lightTheme from "./lightTheme";
-import waifuTheme from "./waifuTheme";
-import darkTheme from "./darkTheme";
+import './i18n'
+import lightTheme from './lightTheme'
+import waifuTheme from './waifuTheme'
+import darkTheme from './darkTheme'
 
-import APR from "./components/apr";
-import InvestSimple from "./components/investSimple";
-import Manage from "./components/manage";
-import Performance from "./components/performance";
-import Zap from "./components/zap";
-import IDai from "./components/idai";
-import Footer from "./components/footer";
-import Home from "./components/home";
-import Header from "./components/header";
-import Vaults from "./components/vault";
-import Dashboard from "./components/dashboard";
-import ConnectWallet from "./components/connectWallet";
-import BetaBanner from "./components/betaBanner";
+import APR from './components/apr'
+import InvestSimple from './components/investSimple'
+import Manage from './components/manage'
+import Performance from './components/performance'
+import Zap from './components/zap'
+import IDai from './components/idai'
+import Footer from './components/footer'
+import Home from './components/home'
+import Header from './components/header'
+import Vaults from './components/vault'
+import Dashboard from './components/dashboard'
+import ConnectWallet from './components/connectWallet'
+import BetaBanner from './components/betaBanner'
 
-import { injected } from "./stores/connectors";
+import { injected } from './stores/connectors'
 
-import { CONNECTION_CONNECTED } from "./constants";
+import { CONNECTION_CONNECTED } from './constants'
 
-import Store from "./stores";
-const emitter = Store.emitter;
-const store = Store.store;
+import Store from './stores'
+const emitter = Store.emitter
+const store = Store.store
 
-const THEMES_LIST = ["dark", "waifu", "light"];
-const LANGS_LIST = ["EN", "DE", "FR", "IT", "JA", "ES", "NL", "PL", "PT", "RU", "ZH"];
+const THEMES_LIST = ['dark', 'waifu', 'light']
+const LANGS_LIST = ['EN', 'DE', 'FR', 'IT', 'JA', 'ES', 'NL', 'PL', 'PT', 'RU', 'ZH']
 
 class App extends Component {
   constructor(props) {
-    super();
+    super()
 
     this.state = {
-      currentTheme: localStorage.getItem("yearnfinewTheme") || THEMES_LIST[0],
-      currentLang: localStorage.getItem("yearnfinewLang") || LANGS_LIST[0],
-      isBeta: localStorage.getItem("yearnfinewBeta") !== "false",
-    };
+      currentTheme: localStorage.getItem('yearnfinewTheme') || THEMES_LIST[0],
+      currentLang: localStorage.getItem('yearnfinewLang') || LANGS_LIST[0],
+      isBeta: localStorage.getItem('yearnfinewBeta') !== 'false',
+    }
   }
 
   setCurrentTheme = (newTheme) => {
-    if (THEMES_LIST.includes(newTheme)) this.setState({ currentTheme: newTheme });
-  };
+    if (THEMES_LIST.includes(newTheme)) this.setState({ currentTheme: newTheme })
+  }
 
   setCurrentLang = (newLang) => {
-    const { i18n } = this.props;
+    const { i18n } = this.props
     if (LANGS_LIST.includes(newLang)) {
-      this.setState({ currentLang: newLang });
-      i18n.changeLanguage(newLang.toLowerCase());
+      this.setState({ currentLang: newLang })
+      i18n.changeLanguage(newLang.toLowerCase())
     }
-  };
+  }
 
   setIsBeta = (beta) => {
-    this.setState({ isBeta: beta });
-  };
+    this.setState({ isBeta: beta })
+  }
 
   componentWillMount() {
     injected.isAuthorized().then((isAuthorized) => {
@@ -68,33 +68,33 @@ class App extends Component {
         injected
           .activate()
           .then((a) => {
-            store.setStore({ account: { address: a.account }, web3context: { library: { provider: a.provider } } });
-            emitter.emit(CONNECTION_CONNECTED);
-            console.log(a);
+            store.setStore({ account: { address: a.account }, web3context: { library: { provider: a.provider } } })
+            emitter.emit(CONNECTION_CONNECTED)
+            console.log(a)
           })
           .catch((e) => {
-            console.log(e);
-          });
+            console.log(e)
+          })
       } else {
       }
-    });
+    })
   }
 
   render() {
-    const themes = { lightTheme, waifuTheme, darkTheme };
-    const { setCurrentTheme, setIsBeta, setCurrentLang } = this;
-    const { currentTheme, isBeta, currentLang } = this.state;
+    const themes = { lightTheme, waifuTheme, darkTheme }
+    const { setCurrentTheme, setIsBeta, setCurrentLang } = this
+    const { currentTheme, isBeta, currentLang } = this.state
     return (
       <MuiThemeProvider theme={createMuiTheme(themes[`${currentTheme}Theme`])}>
         <CssBaseline />
         <IpfsRouter>
           <div
             style={{
-              display: "flex",
-              flexDirection: "column",
-              minHeight: "100vh",
-              alignItems: "center",
-              background: "#f9fafb",
+              display: 'flex',
+              flexDirection: 'column',
+              minHeight: '100vh',
+              alignItems: 'center',
+              background: '#f9fafb',
             }}
           >
             <BetaBanner isBeta={isBeta} setIsBeta={setIsBeta} currentTheme={currentTheme} />
@@ -131,7 +131,8 @@ class App extends Component {
                 <Dashboard />
               </Route>
               <Route path="/">
-                <Home isBeta={isBeta} currentTheme={currentTheme} />
+                <Redirect to="/vaults" />
+                {/* <Home isBeta={isBeta} currentTheme={currentTheme} /> */}
               </Route>
             </Switch>
             <Footer
@@ -144,8 +145,8 @@ class App extends Component {
           </div>
         </IpfsRouter>
       </MuiThemeProvider>
-    );
+    )
   }
 }
 
-export default withNamespaces()(App);
+export default withNamespaces()(App)
